64c5e05a49d1fbe7febdc6868a14c2cb2f6975f7
GScht <gscht@mailbox.org>
2024-03-22 08:32:22 +0100

    Clean up lazygit

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -1,14 +1,4 @@
 return {
   'kdheepak/lazygit.nvim',
-  config = function()
-    print 'Config lazygit'
-    vim.keymap.set('n', '<leader>k', ':LazyGit<cr>', { noremap = true, silent = true })
-  end,
-  cmd = {
-    'LazyGit',
-    'LazyGitConfig',
-    'LazyGitCurrentFile',
-    'LazyGitFilter',
-    'LazyGitFilterCurrentFile',
-  },
+  config = function() end,
 }
