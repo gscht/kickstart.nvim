b4f9f1d8fc763e72e5d34afa6d7db4db286a8022
GScht <gscht@mailbox.org>
2024-10-13 09:19:02 +0200

    Remove fidget

--- init.lua
+++ init.lua
@@ -416,7 +416,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      -- { 'j-hui/fidget.nvim', opts = {} },
     },
     config = function()
       -- Brief Aside: **What is LSP?**
