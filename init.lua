6871a358660e16524e8bbcd2109ead85af59ce5c
Günther Schröttner <guenther@aiaibot.com>
2025-06-09 17:13:07 +0200

    Enable spelling

--- init.lua
+++ init.lua
@@ -53,7 +53,7 @@ vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- Sets how neovim will display certain whitespace in the editor.
+-- Sets how Neovim will display certain white space in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
@@ -74,6 +74,9 @@ vim.opt.colorcolumn = '100'
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
+vim.opt.swapfile = false
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
